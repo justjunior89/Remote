7c024726bf6a317d754c95cc3fb1e2b1ca7b01ab
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-23 17:03:26 -0700

    remove extra end in solved? method

--- board.rb
+++ board.rb
@@ -52,10 +52,10 @@ class Board
   end
 
   def solved?
-    rows.all? { |row| solved_set?(row) } &&
+      rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
-    end
+
   end
 
   def solved_set?(tiles)
